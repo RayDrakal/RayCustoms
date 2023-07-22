0c401241b9c070c09eb534e7faca0d0869272e21
RayDrakal <joonhei0926@naver.com>
2023-07-23 01:26:01 +0900

    update

--- script/c10742500.lua
+++ script/c10742500.lua
@@ -2,8 +2,35 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	c:SetUniqueOnField(1,1,id)
 	--Xyz Summon procedure
 	Xyz.AddProcedure(c,nil,12,5,s.xyzfilter,aux.Stringid(id,0),5,s.xyzop)
+	--immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+	--Multiple attacks
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(s.matkval)
+	c:RegisterEffect(e2)
+	--material
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.mttg)
+	e3:SetOperation(s.mtop)
+	c:RegisterEffect(e3)
 	--Register Special Summons from the Extra Deck
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
@@ -12,6 +39,13 @@ function s.initial_effect(c)
 		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
 	end)
+end
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+function s.matkval(e,c)
+	local oc=e:GetHandler():GetOverlayCount()
+	return math.max(0,oc-1)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	for tc in eg:Iter() do
@@ -39,4 +73,19 @@ function s.xyzop(e,tp,chk)
 	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	Duel.RegisterEffect(e2,tp)
 	return true
+end
+function s.mttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ) 
+		and Duel.IsExistingTarget(Card.TRUE,tp,LOCATION_ONFIELD|LOCATION_GRAVE,LOCATION_ONFIELD|LOCATION_GRAVE,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,Card.TRUE,tp,LOCATION_ONFIELD|LOCATION_GRAVE,LOCATION_ONFIELD|LOCATION_GRAVE,2,2,nil)
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local g=Duel.GetTargetCards(e)
+		if #g>0 then
+			Duel.Overlay(c,g)
+		end
+	end
 end
