9e15571667deb73e497e2dde3569737825f1086d
RayDrakal <joonhei0926@naver.com>
2024-04-15 02:23:50 +0900

    FD Dragonis Start

--- script/RD_init.lua
+++ script/RD_init.lua
@@ -12,6 +12,7 @@
 -- card Scharhrot
 CARD_YOUTH_BERK         = 110810001
 CARD_SCHARHROT          = 110815000
+CARD_FD_DRAGONIS        = 110820000
 
 -- specific functions
 function Auxiliary.fuction1(e,tp)
