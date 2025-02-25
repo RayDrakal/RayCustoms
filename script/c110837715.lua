9b4e71076f10bfece822f16da3d887641ae30c90
RayDrakal <joonhei0926@naver.com>
2025-02-25 12:45:03 +0900

    bug fixed

--- script/c110837715.lua
+++ script/c110837715.lua
@@ -1,5 +1,18 @@
 --이매지너리 아크 턴오버
 local s,id=GetID()
 function s.initial_effect(c)
+	--cannot be target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.target)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
 	
+end
+function s.target(e,c)
+	return c:IsSetCard(SET_METARION)
 end
