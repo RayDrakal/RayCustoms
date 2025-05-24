cee2da08c027b75cf39b8bc90c013c8b6c3f9ae2
RayDrakal <joonhei0926@naver.com>
2025-05-24 19:04:56 +0900

    discard

--- script/RD_init.lua
+++ script/RD_init.lua
@@ -14,3 +14,11 @@ CARD_YOUTH_BERK         = 110810001
 CARD_SCHARHROT          = 110815000
 
 -- specific functions
+function Auxiliary.func1(e,tp)
+    
+end
+
+settt={}
+function settt.fuc1(c,tp)
+    
+end
