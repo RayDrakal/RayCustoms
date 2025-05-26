7e6fb38723c2ed288ab2a0478ccc68f70bcba8ea
RayDrakal <joonhei0926@naver.com>
2025-05-26 22:23:13 +0900

    bug fix

--- script/c110750000.lua
+++ script/c110750000.lua
@@ -74,7 +74,7 @@ function s.attop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local tc=Duel.SelectMatchingCard(tp,s.attfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_EXTRA,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED,1,1,nil,e):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.attfilter,tp,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e):GetFirst()
 	if tc then
 		Duel.Overlay(c,tc,true)
 	end
