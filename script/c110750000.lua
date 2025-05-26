81aed5107c9eec9d49fb3e180dbb72b26ceda135
RayDrakal <joonhei0926@naver.com>
2025-05-26 22:17:36 +0900

    Krimson

---
+++ script/c110750000.lua
@@ -0,0 +1,81 @@
+--네가로기어 아제우스 END
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Xyz Summon procedure
+	Xyz.AddProcedure(c,nil,13,3,s.ovfilter,aux.Stringid(id,0))
+	--immune
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.imcon)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+	--Send cards to GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_END_PHASE)
+	e2:SetCost(Cost.Detach(1,1,nil))
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
+	c:RegisterEffect(e2)
+	--Attach 1 card to itself
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_DELAY,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.attcon)
+	e3:SetTarget(s.atttg)
+	e3:SetOperation(s.attop)
+	c:RegisterEffect(e3)
+	
+end
+function s.imfilter(c)
+	return c:IsRank(12) and c:IsType(TYPE_XYZ)
+end
+function s.imcon(e)
+	return e:GetHandler():GetOverlayGroup():IsExists(s.imfilter,1,nil)
+end
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+function s.ovfilter(c,tp,xyzc)
+	return c:IsFaceup() and c:GetRank()==12 and c:GetOverlayCount()==0
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SendtoGrave(g,REASON_EFFECT)
+end
+function s.attfilter(c,tp)
+	return c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.attcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.attfilter,1,nil,tp)
+end
+function s.atttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.attfilter,tp,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED,1,nil,e)
+		and e:GetHandler():IsType(TYPE_XYZ) end
+end
+function s.attop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local tc=Duel.SelectMatchingCard(tp,s.attfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_EXTRA,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED,1,1,nil,e):GetFirst()
+	if tc then
+		Duel.Overlay(c,tc,true)
+	end
+end
